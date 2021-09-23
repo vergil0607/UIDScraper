4f06a3120c5a4b96145838f2846de5f6c0c9ac5e
unknown <sebastian.wieser2341@gmail.com>
2021-09-23 08:36:25 +0200

    make Paths relative

--- start_UID_check.bat
+++ start_UID_check.bat
@@ -1,12 +1,17 @@
 
 
 set original_dir=%CD%
-set venv_root_dir="C:\Users\Wieser\Documents\Projekte\TAG\2021\RPA\UID Use Case\202106_Python\venv"
+
+set venv_root_dir=%original_dir%\venv
 cd %venv_root_dir%
-call %venv_root_dir%\Scripts\activate.bat
+
+call Scripts\activate.bat
+
+set python_path = %original_dir% and "venv\Scripts\python.exe"
+set python_file = %original_dir% and "UIDBOT\ValidateUIDs.py"
 
 
-"C:\Users\Wieser\Documents\Projekte\TAG\2021\RPA\UID Use Case\202106_Python\venv\Scripts\python.exe" "C:\Users\Wieser\Documents\Projekte\TAG\2021\RPA\UID Use Case\202106_Python\UIDBOT\ValidateUIDs.py"
+"%original_dir%\venv\Scripts\python.exe" "%original_dir%\UIDBOT\ValidateUIDs.py"
 
 pause
 
