b297a809f54483d4ad27ff19fae352e3167026e3
unknown <sebastian.wieser2341@gmail.com>
2021-10-04 21:33:02 +0200

    add attempting loop

--- UIDBOT/ValidateUIDs.py
+++ UIDBOT/ValidateUIDs.py
@@ -11,7 +11,7 @@ class ValidateUIDsFromExcel:
         self.validation.acceptCookies()
         self.validation.close_windows()
         self.input = r"C:\Users\Wieser\Documents\Projekte\TAG\2021\RPA\UID Use " \
-                     r"Case\202106_Python\UIDBOT\ValidatedUIDs.xlsx "
+                     r"Case\202106_Python\UIDBOT\ValidatedUIDs.xlsx"
         self.workbook = openpyxl.load_workbook(self.input)
         self.inputSheet = self.workbook['Table 1']
         self.numberofrows = len(self.inputSheet['A'])
@@ -20,7 +20,13 @@ class ValidateUIDsFromExcel:
 
     def validateUIDsInExcel(self, maxUID=100):
         """loop through the rows of the Excel file"""
-        for row in self.inputSheet.iter_rows(min_row=2, max_row=maxUID):
+        for j, row in enumerate(self.inputSheet.iter_rows(min_row=2, max_row=maxUID)):
+            if j%10 == 0:
+                print("{}/{} checked".format(j, self.numberofrows))
+            self.tryScarping(row)
+
+    def tryScarping(self, row, max_tries=3):
+        for i in range(max_tries):
             try:
                 print(row[0].value)
                 if row[0].value is None:
@@ -30,13 +36,19 @@ class ValidateUIDsFromExcel:
                 else:
                     row[5].value = 'not valid'
                 print(row[5].value)
+                break
             except:
                 row[5].value = 'please check manually'
+                continue
 
     def saveExcel(self):
         """save update of Excel file"""
         self.workbook.save(filename=r"C:\Users\Wieser\Documents\Projekte\TAG\2021\RPA\UID Use "
                                     r"Case\202106_Python\UIDBOT\ValidatedUIDs_Output.xlsx")
+
+    def log(self, msg):
+        with open('out.txt', 'a') as f:
+            print(msg, '\n', file=f)
 
 
 if __name__ == "__main__":
